004c1c61742ea3cbcf8492c818d7d7f863129f09
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-05-24 19:00:41 +0300

    Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>JCI</title>
+        <meta name='description' content='JCI site' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <div>
+          <p>Beginning</p>
+        </div>
+      </main>
+    </>
+  );
+}
