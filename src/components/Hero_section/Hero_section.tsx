09f5886a7c5906ce22d979824a118c8d1bb90b84
vitaliybelov.bvs <vitaliybelov.bvs@gmail.com>
2023-06-16 02:09:03 +0300

    slider add

--- src/components/Hero_section/Hero_section.tsx
+++ src/components/Hero_section/Hero_section.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import css from './Hero_section.module.css';
 import { MainButton } from '../ui-kit/buttons/MainButton/MainButton';
+import HeroSlider from '../ui-kit/sliders/HeroSlider/HeroSlider';
 
 
+interface Image {
+  src: string;
+  alt: string;
+  resolutions: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+}
 
+const images: Image[] = [
+  {
+    src: "/images/hero/hero_01d.jpg",
+    alt: "Image 1",
+    resolutions: {
+      small: "/images/hero/hero_01m.jpg",
+      medium: "/images/hero/hero_01t.jpg",
+      large: "/images/hero/hero_01d.jpg",
+    },
+  },
+  {
+    src: "/images/hero/hero_02d.jpg",
+    alt: "Image 2",
+    resolutions: {
+      small: "/images/hero/hero_02m.jpg",
+      medium: "/images/hero/hero_02t.jpg",
+      large: "/images/hero/hero_02d.jpg",
+    },
+  },
+  {
+    src: "/images/hero/hero_03d.jpg",
+    alt: "Image 3",
+    resolutions: {
+      small: "/images/hero/hero_03m.jpg",
+      medium: "/images/hero/hero_03t.jpg",
+      large: "/images/hero/hero_03d.jpg",
+    },
+  },
+];
 
 
 export const HeroSection = () => {
@@ -13,7 +52,7 @@ export const HeroSection = () => {
       <MainButton type='button' text='Join us' style='primary' paddings={60} />
       <MainButton type='button' text='Donate' style='secondary' paddings={150} />
       <MainButton type='button' text='Read more' style='primary' />
-
+      <HeroSlider images={images}/>
     </div>
    
   );
---
+++ src/components/ui-kit/sliders/HeroSlider/HeroSlider.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import styles from './HeroSlider.module.css';
+import { Pagination } from "swiper";
+
+
+interface Image {
+  src: string;
+  alt: string;
+  resolutions: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+}
+
+interface HeroSliderProps {
+  images: Image[];
+}
+
+const HeroSlider: React.FC<HeroSliderProps> = ({ images }) => {
+return (
+    <>
+      <Swiper
+        pagination={{
+          dynamicBullets: false,
+        }}
+        modules={[Pagination]}
+        className={styles.heroSlider}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index} className={styles.heroSlide}>
+            <picture>
+              <source srcSet={image.resolutions.large} media="(min-width: 1440px)" />
+              <source srcSet={image.resolutions.medium} media="(min-width: 768px)" />
+              <img src={image.resolutions.small} alt={image.alt} />
+            </picture>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default HeroSlider;
