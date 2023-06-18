6afc732a2dd08d26d80819c3666133277962e195
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-06-18 18:00:51 +0300

    Init FAQ

--- src/components/Hero_section/Hero_section.tsx
+++ src/components/Hero_section/Hero_section.tsx
@@ -1,19 +1,27 @@
+import { Container } from '../Container/Container';
 import { MainButton } from '../ui-kit/buttons/MainButton/MainButton';
 import HeroSlider from '../ui-kit/sliders/HeroSlider/HeroSlider';
 import { images } from './images';
 
 export const HeroSection = () => {
   return (
-    <div>
-      <MainButton type='button' text='Join us' style='primary' paddings={60} />
-      <MainButton
-        type='button'
-        text='Donate'
-        style='secondary'
-        paddings={150}
-      />
-      <MainButton type='button' text='Read more' style='primary' />
-      <HeroSlider images={images} />
-    </div>
+    <section>
+      <Container>
+        <MainButton
+          type='button'
+          text='Join us'
+          style='primary'
+          paddings={60}
+        />
+        <MainButton
+          type='button'
+          text='Donate'
+          style='secondary'
+          paddings={150}
+        />
+        <MainButton type='button' text='Read more' style='primary' />
+        <HeroSlider images={images} />
+      </Container>
+    </section>
   );
 };
