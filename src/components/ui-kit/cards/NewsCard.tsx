b1b5a3b4eff84238c9ad63e55c25d7baa94e2ea0
vitaliybelov.bvs <vitaliybelov.bvs@gmail.com>
2023-06-20 00:53:39 +0300

    add: newsSlider-desktop version

--- src/components/ui-kit/cards/NewsCard.tsx
+++ src/components/ui-kit/cards/NewsCard.tsx
@@ -20,14 +20,50 @@ interface Card {
   text: string;
 }
 
-interface NewsCardProps {
-  cards: Card[];
-}
+// interface NewsCardProps {
+//   cards: Card[];
+// }
 
-export const NewsCard: React.FC<NewsCardProps> = ({ cards }) => {
+interface NewsCardProps {
+  card: Card;
+}
+export const NewsCard: React.FC<NewsCardProps> = ({ card }) => {
   return (
     <>
-      <div className={s.newsCards}>
+      <div className={s.card}>
+        <div className={s.picture}>
+          <picture>
+            <source
+              srcSet={card.picture.resolutions.large}
+              media='(min-width: 1440px)'
+            />
+            <source
+              srcSet={card.picture.resolutions.medium}
+              media='(min-width: 768px)'
+            />
+            <img
+              src={card.picture.resolutions.small}
+              alt={card.picture.alt}
+            />
+          </picture>
+        </div>
+        <div className={s.textblock}>
+          <div className={s.data}>
+            <div className={s.datePrimary}>
+              <p className={s.date}>{card.data.date}</p>
+              <span className={s.decor}>|</span>
+              <p className={s.location}>{card.data.location}</p>
+            </div>
+            <span className={s.decorCircle}>&#8226;</span>
+            <p className={s.level}>{card.data.level}</p>
+          </div>
+          <div className={s.info}>
+            <h3 className={s.title}>{card.title}</h3>
+            <p className={s.text}>{card.text}</p>
+          </div>
+        </div>
+      </div>
+      {/* <div className={s.newsCards}>
         {cards.map((card, index) => (
           <div key={index} className={s.card}>
             <div className={s.picture}>
@@ -64,7 +100,7 @@ export const NewsCard: React.FC<NewsCardProps> = ({ cards }) => {
           </div>
         ))}
         ;
-      </div>
+      </div> */}
     </>
   );
 };
