d14d6bd72cfcc9f91eed874ad9689019e1d47109
andrey-lawyer <andrey-1@ukr.net>
2023-07-20 14:14:25 +0300

    add lightbox

--- src/sections/PhotoGallerySection/PhotoGallerySection.tsx
+++ src/sections/PhotoGallerySection/PhotoGallerySection.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
+import { useState } from 'react';
 import { SwiperSlide } from 'swiper/react';
+import Lightbox from 'yet-another-react-lightbox';
 import dynamic from 'next/dynamic';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'yet-another-react-lightbox/styles.css';
 
 const MediaQuery = dynamic(() => import('react-responsive'), {
   ssr: false,
@@ -12,10 +16,14 @@ import s from './PhotoGallerySection.module.css';
 
 import { Container } from '@/components/Container';
 import { images, imagesForDesk } from './images';
+import LeftArrow from 'public/icons/chevron_Left.svg';
+import RightArrow from 'public/icons/chevron_Right.svg';
 import { useScreen } from '@/hooks/use_screen';
 
 export const PhotoGallerySection = () => {
   const { isMobile } = useScreen();
+  const [index, setIndex] = useState(-1);
+
   const width = isMobile ? 252 : 288;
 
   return (
@@ -34,6 +42,7 @@ export const PhotoGallerySection = () => {
             {images.map(({ src, alt }, index) => (
               <SwiperSlide key={index}>
                 <Image
+                  onClick={() => setIndex(index)}
                   className={s.picture}
                   src={src}
                   width={width}
@@ -47,6 +56,34 @@ export const PhotoGallerySection = () => {
             ))}
           </div>
         </div>
+        <Lightbox
+          className={s.light_box}
+          render={{
+            iconPrev: () => {
+              return (
+                <div className={s.box_arrow}>
+                  <LeftArrow className={s.arrow} />
+                </div>
+              );
+            },
+            iconNext: () => {
+              return (
+                <div className={s.box_arrow}>
+                  <RightArrow className={s.arrow} />
+                </div>
+              );
+            },
+          }}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          slides={images}
+          styles={{
+            container: {
+              backgroundColor: 'rgba(0, 0, 0, 0.6)',
+            },
+          }}
+        />
       </MediaQuery>
 
       <MediaQuery minWidth={1440}>
@@ -61,6 +98,7 @@ export const PhotoGallerySection = () => {
             {imagesForDesk.map(({ src, alt, heightDeck }, index) => (
               <li className={s.gallery__grid__item} key={index}>
                 <Image
+                  onClick={() => setIndex(index)}
                   className={s.picture}
                   src={src}
                   width={360}
@@ -73,6 +111,38 @@ export const PhotoGallerySection = () => {
             ))}
           </ul>
         </Container>
+        <Lightbox
+          className={s.light_box}
+          render={{
+            iconPrev: () => {
+              return (
+                <div className={s.box_arrow}>
+                  <LeftArrow className={s.arrow} />
+                </div>
+              );
+            },
+            iconNext: () => {
+              return (
+                <div className={s.box_arrow}>
+                  <RightArrow className={s.arrow} />
+                </div>
+              );
+            },
+          }}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          slides={imagesForDesk}
+          styles={{
+            container: {
+              backgroundColor: 'rgba(0, 0, 0, 0.6)',
+            },
+            icon: {
+              width: '60px',
+              height: '60px',
+            },
+          }}
+        />
       </MediaQuery>
     </section>
   );
