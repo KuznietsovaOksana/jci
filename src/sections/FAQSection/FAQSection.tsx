c28de68db31b5e73f03c2c28fdf9443c49a1fda5
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-07-22 16:35:33 +0300

    Add Title component

--- src/sections/FAQSection/FAQSection.tsx
+++ src/sections/FAQSection/FAQSection.tsx
@@ -2,6 +2,7 @@ import { Container } from '@/components/Container';
 import { Accordion } from '@/components/Accordion';
 import { Section } from '@/components/Section';
 import { faqs } from '@/components/Accordion/data';
+import { Title } from '@/components/Title';
 
 import s from './FAQSection.module.css';
 
@@ -10,9 +11,9 @@ export const FAQSection = () => {
     <Section className={s.section}>
       <Container>
         <div className={s.box}>
-          <h2 className={s.title}>
+          <Title className={s.title}>
             Frequently asked <span className={s.blue}>questions</span>
-          </h2>
+          </Title>
           <Accordion faqs={faqs} />
         </div>
       </Container>
