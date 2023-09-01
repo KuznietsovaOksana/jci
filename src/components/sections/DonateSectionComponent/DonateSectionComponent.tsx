dee914f5fda938b6ad5d4679471577907b0d15b1
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-09-02 00:29:04 +0300

    Add localization to Donate and News

--- src/components/sections/DonateSectionComponent/DonateSectionComponent.tsx
+++ src/components/sections/DonateSectionComponent/DonateSectionComponent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'next-i18next';
 
 import { Container } from '@/components/common/Container';
 import { MainButton } from '@/components/buttons/MainButton';
@@ -8,13 +9,15 @@ import { TextProps } from './DonateSectionComponent.props';
 import s from './DonateSectionComponent.module.css';
 
 export const DonateSectionComponent: React.FC<TextProps> = ({ heading }) => {
+  const { t } = useTranslation('common');
+
   return (
     <section className={s.donate_section}>
       <Container>
         <Title className={s.donate_text}>{heading}</Title>
         <MainButton
           className={s.donate_btn}
-          text='Donate'
+          text={t('buttons.donate')}
           style='secondaryNavy'
         />
       </Container>
