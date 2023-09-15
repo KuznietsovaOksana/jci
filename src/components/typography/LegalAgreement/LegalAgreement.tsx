5644ff4740a2436c4e59bf47c3371733d3754881
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-09-15 23:22:30 +0300

    Add styles

--- src/components/typography/LegalAgreement/LegalAgreement.tsx
+++ src/components/typography/LegalAgreement/LegalAgreement.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 
 import { LegalAgreementProps } from './LegalAgreement.props';
+
 import s from './LegalAgreement.module.css';
 
 export const LegalAgreement: FC<LegalAgreementProps> = ({
@@ -11,21 +12,22 @@ export const LegalAgreement: FC<LegalAgreementProps> = ({
   const TitleTag = titleType || 'h3';
 
   return (
-    <div>
+    <div className={s.component}>
       <TitleTag>{title}</TitleTag>
       {content.map((item, index) => {
         if (item.type === 'paragraph') {
           const isLastParagraph = index === content.length - 1;
-          const paragraphStyle = isLastParagraph ? '' : 'margin';
+          const paragraphStyle = isLastParagraph ? s.noMargin : s.margin;
+          const combinedClassName = `${s.text} ${paragraphStyle}`;
 
           return (
-            <p key={index} className={paragraphStyle}>
+            <p key={index} className={combinedClassName}>
               {item.text}
             </p>
           );
         } else if (item.type === 'list') {
           return (
-            <ul key={index}>
+            <ul key={index} className={s.margin}>
               {item?.items?.map((listItem, subIndex) => (
                 <li key={subIndex}>{listItem}</li>
               ))}
