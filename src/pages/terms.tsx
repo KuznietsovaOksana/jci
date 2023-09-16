3e0eb5f238d6b556ba0636d281ad24c38912ef31
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-09-16 13:16:41 +0300

    Add TermsPage content

---
+++ src/pages/terms.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Head from 'next/head';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { Portal } from '@/components/common/Portal';
+import { ModalMenu } from '@/components/header/ModalMenu';
+import { Layout } from '@/layout/Layout';
+import TermsSection from '@/sections/TermsPage/TermsSection/TermsSection';
+
+export default function Terms() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <title>JCI Terms of Service</title>
+        <meta name='description' content='JCI Terms of Service' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      {showModal && (
+        <Portal>
+          <ModalMenu setShowModal={setShowModal} />
+        </Portal>
+      )}
+      <Layout setShowModal={setShowModal}>
+        <main>
+          <TermsSection />
+        </main>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'en', [
+        'common',
+        'navigation',
+        'termsPage',
+      ])),
+    },
+  };
+}
