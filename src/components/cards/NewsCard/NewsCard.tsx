c1a87cf916aa0a885d03c755c674ad9ffb7c12f7
Vitalii Bielov <vitaliybelov.bvs@gmail.com>
2023-11-26 21:42:09 +0200

    deleted coments

--- src/components/cards/NewsCard/NewsCard.tsx
+++ src/components/cards/NewsCard/NewsCard.tsx
@@ -21,9 +21,6 @@ interface Card {
   text: string;
 }
 
-// interface NewsCardProps {
-//   cards: Card[];
-// }
 
 interface NewsCardProps {
   card: Card;
@@ -62,75 +59,6 @@ export const NewsCard: React.FC<NewsCardProps> = ({ card }) => {
         </div>
       </a>
 
-      {/* <div className={s.card} tabIndex={1} >
-        <div className={s.picture}>
-          <picture>
-            <source
-              srcSet={card.picture.resolutions.large}
-              media='(min-width: 1440px)'
-            />
-            <source
-              srcSet={card.picture.resolutions.medium}
-              media='(min-width: 768px)'
-            />
-            <img src={card.picture.resolutions.small} alt={card.picture.alt} />
-          </picture>
-        </div>
-        <div className={s.textblock}>
-          <div className={s.data}>
-            <div className={s.datePrimary}>
-              <p className={s.date}>{card.data.date}</p>
-              <span className={s.decor}>|</span>
-              <p className={s.location}>{card.data.location}</p>
-            </div>
-            <span className={s.decorCircle}>&#8226;</span>
-            <p className={s.level}>{card.data.level}</p>
-          </div>
-          <div className={s.info}>
-            <h3 className={s.title}>{card.title}</h3>
-            <p className={s.text}>{card.text}</p>
-          </div>
-        </div>
-      </div> */}
-
-      {/* <div className={s.newsCards}>
-        {cards.map((card, index) => (
-          <div key={index} className={s.card}>
-            <div className={s.picture}>
-              <picture>
-                <source
-                  srcSet={card.picture.resolutions.large}
-                  media='(min-width: 1440px)'
-                />
-                <source
-                  srcSet={card.picture.resolutions.medium}
-                  media='(min-width: 768px)'
-                />
-                <img
-                  src={card.picture.resolutions.small}
-                  alt={card.picture.alt}
-                />
-              </picture>
-            </div>
-            <div className={s.textblock}>
-              <div className={s.data}>
-                <div className={s.datePrimary}>
-                  <p className={s.date}>{card.data.date}</p>
-                  <span className={s.decor}>|</span>
-                  <p className={s.location}>{card.data.location}</p>
-                </div>
-                <span className={s.decorCircle}>&#8226;</span>
-                <p className={s.level}>{card.data.level}</p>
-              </div>
-              <div className={s.info}>
-                <h3 className={s.title}>{card.title}</h3>
-                <p className={s.text}>{card.text}</p>
-              </div>
-            </div>
-          </div>
-        ))}
-        ;
-      </div> */}
     </>
   );
 };
